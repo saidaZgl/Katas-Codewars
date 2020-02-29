8846d3daa2edccdfdbd8a91f56e76338955fbf51
Saida Zghal <zghal.saida@orange.fr>
2020-02-29 13:40:04 +0100

    solution kata mumbling

--- mumbling.js
+++ mumbling.js
@@ -10,3 +10,16 @@ accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 */
+
+function accum(s) {
+  let letters = s.toLowerCase().split("");
+  console.log(letters);
+  for (let i = 0; i < letters.length; i++) {
+    console.log(letters[i]);
+    letters[i] = letters[i].toUpperCase() + letters[i].repeat(i);
+    console.log(letters[i]);
+  }
+  return letters.join("-");
+}
+
+accum("eMlet");
